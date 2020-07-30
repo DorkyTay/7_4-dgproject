79c789f2722585cd20c952b5bae300122b6f36c1
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-30 10:29:02 -0400

    Add project files.

---
+++ Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_4_dgproject
+{
+    public class Message
+    {
+        public DatingProfile sender { get; set; }
+        public string messageTitle { get; set; }
+
+        public string messageData { get; set; }
+
+        public bool isRead { get; set; }
+
+        public Message(DatingProfile sender, string title, string message)
+        {
+
+            this.sender = sender;
+            this.messageTitle = title;
+            this.messageData = message;
+            this.isRead = false;
+
+        }
+
+    }
+}
